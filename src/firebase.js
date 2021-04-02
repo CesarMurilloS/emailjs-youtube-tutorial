f946abe4220d581067f531a49950531a1650612c
Cesar Yael Murillo Sandoval <murilloscesar@gmail.com>
2021-04-02 11:38:51 -0600

    create-firebase-config-files-deploy-to-firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC6E3f2LvqaCece8W-wjGyW6S7gEa7JIDo",
+    authDomain: "emailjs--tutorial.firebaseapp.com",
+    projectId: "emailjs--tutorial",
+    storageBucket: "emailjs--tutorial.appspot.com",
+    messagingSenderId: "774210259857",
+    appId: "1:774210259857:web:dd9e811aebbafb55092830",
+    measurementId: "G-WV3QG7T0WJ"
+  };
